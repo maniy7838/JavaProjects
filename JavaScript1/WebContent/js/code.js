48ffae4e8029089bf8ebd1ddf60a3190560ef068
Mani Yadav <my10@iitbbs.ac.in>
2018-08-02 19:27:15 +0530

    JavaScript

---
+++ JavaScript1/WebContent/js/code.js
@@ -0,0 +1,23 @@
+
+function display(){
+	var hd = document.getElementById('hellodiv');
+	hd.innerHTML = 'Clicked......';
+}/*
+var myFunction = function(var1, var2) {
+	return (var1 + var2);
+}
+function greetings() {
+	var sum = myFunction(10, 20);
+	alert(sum);
+}
+
+function test1(){
+	var var1 = 1;
+}
+
+test1();
+
+function test2(){
+	alert(var1);
+}
+test2();*/
